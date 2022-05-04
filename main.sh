5bab13c3a1bba21025bf68fc670647deaaf8364a
prinzjoseph <princejoseph1k94@gmail.com>
2022-05-04 10:16:02 +0530

    v1.0

--- main.sh
+++ main.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#Author: prinzjoseph
+# Script to Install Wordpress with LAMP Stack on Ubuntu
+
+printf "\e[1;94m __        ______            _   _ ____  _   _ _   _ _____ _   _  \e[0m\n" 
+printf "\e[1;94m \ \      / /  _ \          | | | | __ )| | | | \ | |_   _| | | | \e[0m\n"
+printf "\e[1;94m  \ \ /\ / /| |_) |  _____  | | | |  _ \| | | |  \| | | | | | | | \e[0m\n"
+printf "\e[1;94m   \ V  V / |  __/  |_____| | |_| | |_) | |_| | |\  | | | | |_| | \e[0m\n"
+printf "\e[1;94m    \_/\_/  |_|              \___/|____/ \___/|_| \_| |_|  \___/  \e[0m\n"
 
 source ./lamp.sh
 source ./wp.sh
